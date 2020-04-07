33c36f8ccde7967b3b31b9aa3d5bcceaf6b9caea
Allen Serhat <serhata@gmail.com>
2020-04-06 20:21:43 -0400

    Adding VM

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "vsphere" {
-  user           = "user"
-  password       = "pass"
-  vsphere_server = "35.222.146.209:8989"
+  user           = var.vcsim_vc_user
+  password       = var.vcsim_vc_pass
+  vsphere_server = "34.69.0.68:8989"
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
--- variables.tf
+++ variables.tf
@@ -1,1 +1,3 @@
 variable "gcp_creds" {}
+variable "vcsim_vc_user" {}
+variable "vcsim_vc_pass" {}
